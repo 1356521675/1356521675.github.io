6a575ffd0ff31e51df9f89854f5760a4debe9e27
张master <1356521675@qq.com>
2020-05-22 15:45:59 +0800

    commit

---
+++ precache-manifest.3d94d8b326ace3e47cc00b4502e7a8a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e4ca21d96d6dd484809a6f86813c7e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "306827406e9c50f4978e",
+    "url": "/static/css/main.f0f54787.chunk.css"
+  },
+  {
+    "revision": "c29e9943ee4098679e16",
+    "url": "/static/js/2.60c333f8.chunk.js"
+  },
+  {
+    "revision": "306827406e9c50f4978e",
+    "url": "/static/js/main.727aece1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "648d82d0a1ab0c34b4ddb2766f1bc572",
+    "url": "/static/media/my_logo.648d82d0.jpg"
+  }
+]);
